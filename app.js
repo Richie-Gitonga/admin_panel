0a0c1029f67dc0e3e7328636f65145c24f0bd550
Richie-Gitonga <gitongar00@gmail.com>
2023-11-27 10:41:54 +0300

    added js to toggle from dark to light mode and to display or close sidebar

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const sideBar = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn')
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideBar.style.display = "block"
+})
+
+closeBtn.addEventListener('click', () => {
+    sideBar.style.display = "none"
+})
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+})
+
 orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
